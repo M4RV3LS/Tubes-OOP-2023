6d9fc583c084beb57cff187ca45e6164ec963fff
Marvel Subekti <marvelsubekti@gmail.com>
2023-05-01 03:54:37 +0700

    update House

--- Map/House.java
+++ Map/House.java
@@ -1,5 +1,5 @@
 package Map;
-import java.util.ArrayList;
+import java.util.*;
 
 public class House {
     private ArrayList<Room> rooms;
@@ -34,5 +34,71 @@ public class House {
             }
         }
         return null;
+    }
+
+    public void upgradeHouse(Room newrRoom) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tolong pilih ruangan mana yang ingin dipakai sebagai acuan upgrade rumah: ");
+        for (int i = 0; i < rooms.size(); i++) {
+            System.out.println((i+1) + ". " + rooms.get(i).getRoomName());
+        }
+
+        int selectedRoomIndex = scanner.nextInt()-1;
+        Room selectedRoom = rooms.get(selectedRoomIndex);
+        System.out.println("Please select the position to add the room:");
+        System.out.println("1. Top");
+        System.out.println("2. Right");
+        System.out.println("3. Bottom");
+        System.out.println("4. Left");
+        int selectedPosition = scanner.nextInt();
+        switch (selectedPosition) 
+        {
+            case 1:
+                if(selectedRoom.getRoomUp() == null) 
+                {
+                    selectedRoom.setRoomUp(newrRoom);
+                } 
+                
+                else {
+                    System.out.println("There is already a room there.");
+                }
+                break;
+            
+            case 2:
+                if(selectedRoom.getRoomRight() == null) 
+                {
+                    selectedRoom.setRoomRight(newrRoom);
+                } 
+                
+                else {
+                    System.out.println("There is already a room there.");
+                }
+            break;
+            
+            case 3:
+                if(selectedRoom.getRoomDown() == null) 
+                {
+                    selectedRoom.setRoomDown(newrRoom);
+                } 
+                
+                else {
+                    System.out.println("There is already a room there.");
+                }
+            break;
+            
+            case 4:
+                if(selectedRoom.getRoomLeft() == null) 
+                {
+                    selectedRoom.setRoomLeft(newrRoom);
+                } 
+                
+                else {
+                    System.out.println("There is already a room there.");
+                }
+                break;
+            
+            default:
+                System.out.println("Invalid input.");
+        }
     }
 }
