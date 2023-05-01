657b4383d51da19b10b2ce3d141948fe89dc67d6
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2023-05-01 14:24:12 +0700

    ini help

---
+++ Help.java
@@ -0,0 +1,19 @@
+public class Help {
+    public static void main(String[] args) {
+        System.out.println("-Help-");
+        System.out.println("Sim-Plicity adalah permainan simulasi kehidupan yang berbasis Command Line Interface (CLI).");
+        System.out.println("Pemain akan mengendalikan sejumlah sim yang harus dijaga kesejahteraannya agar tidak depresi dan mati.");
+        System.out.println("Pemain harus memenuhi kebutuhan mood, kekenyangan, dan kesehatan dari setiap sim untuk menjaga kesejahteraannya.");
+        System.out.println("Jumlah sim yang dapat dimainkan tergantung dari berapa sim yang dibuat pada awal permainan.");
+        System.out.println();
+        System.out.println("Aturan Permainan");
+        System.out.println("1. Masukkan jumlah sim yang ingin dimainkan, kemudian masukkan nama-nama sim yang ingin dibuat");
+        System.out.println("2. Setiap sim akan memiliki kebutuhan berupa mood, kekenyangan, dan kesehatan");
+        System.out.println("3. Pemain harus memenuhi kebutuhan tersebut dengan memberi perintah yang tepat melalui CLI");
+        System.out.println("4. Pemain dapat memberikan perintah untuk memberi makan, minum, membersihkan, merawat kesehatan, atau memberikan hiburan kepada setiap sim");
+        System.out.println("5. Pemain harus memperhatikan level kebutuhan setiap sim dan segera memberikan perintah yang tepat untuk menghindari kematian atau depresi pada sim");
+        System.out.println("6. Permainan berakhir jika semua sim meninggal atau depresi");
+        System.out.println();
+        System.out.println("Selamat bermain Sim-Plicity!");
+        }
+    }
