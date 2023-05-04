85d5f0fa16ee433104b11bbdee138f5add0b726a
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2023-05-05 05:03:41 +0700

    Update Load.java

--- Load.java
+++ Load.java
@@ -1,41 +1,99 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Load {
-    private String filename;
-    private String data;
+    private FileReader file;
+    private ArrayList<Sim> simList;
+    private World world;
 
-    public Load(String filename) {
-        this.filename = filename;
-    }
-
-    public void load(String path) {
+    public Load(ArrayList<Sim> simList, World world, String filename) {
+        this.simList = simList;
+        this.world = world;
         try {
-            File file = new File(path + "\\" + filename);
-            FileReader reader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            String line;
-            StringBuilder stringBuilder = new StringBuilder();
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-            data = stringBuilder.toString();
-            bufferedReader.close();
-            reader.close();
-            System.out.println("Data berhasil dimuat dari file " + file.getAbsolutePath());
+            file = new FileReader(filename);
         } catch (IOException e) {
-            System.out.println("Terjadi kesalahan dalam membaca file " + filename);
+            System.out.println("File tidak ditemukan.");
             e.printStackTrace();
         }
     }
 
-    public String getData() {
-        return data;
+    public void loadGame() {
+        BufferedReader reader = new BufferedReader(file);
+
+        try {
+            /* load sims */
+            String line = reader.readLine();
+            while (line != null) {
+                Sim sim = new Sim();
+                sim.setNameLengkap(line);
+                sim.setPekerjaan(new Pekerjaan(reader.readLine()));
+                sim.setUang(Integer.parseInt(reader.readLine()));
+                
+                // read bahan makanan
+                // read furniture
+                // read masakan
+
+                sim.setKekenyangan(Integer.parseInt(reader.readLine()));
+                sim.setMood(Integer.parseInt(reader.readLine()));
+                sim.setKesehatan(Integer.parseInt(reader.readLine()));
+                sim.setStatus(reader.readLine());
+
+                String houseName = reader.readLine();
+                House house = world.getHouseByName(houseName);
+                if (house == null) {
+                    house = new House(houseName);
+                    world.addHouse(house);
+                }
+                sim.setHouse(house);
+
+                ArrayList<Room> rooms = house.getRooms();
+                for (Room elmt : rooms) {
+                    String roomName = reader.readLine();
+                    if (roomName.equals(elmt.getRoomName())) {
+                        sim.setCurrentRoom(elmt);
+                        break;
+                    }
+                }
+
+                // read detail room (lokasi objek dsb)
+
+                simList.add(sim);
+                line = reader.readLine();
+            }
+            
+            /* load world */
+            line = reader.readLine();
+            while (line != null) {
+                String houseName = line;
+                int[] loc = parseLocationString(reader.readLine());
+                House house = world.getHouseByName(houseName);
+                if (house == null) {
+                    house = new House(houseName);
+                    world.addHouse(house);
+                }
+                world.setHouseLocation(house, loc[0], loc[1]);
+                line = reader.readLine();
+            }
+
+            System.out.println("File berhasil diload.");
+            reader.close();
+            file.close();
+
+        } catch (IOException e) {
+            System.out.println("File gagal diload.");
+            e.printStackTrace();
+        }
     }
 
-    public void setData(String data) {
-        this.data = data;
+    private int[] parseLocationString(String locationString) {
+        String[] tokens = locationString.split(",");
+        int[] loc = new int[2];
+        loc[0] = Integer.parseInt(tokens[0].trim());
+        loc[1] = Integer.parseInt(tokens[1].trim());
+        return loc;
     }
 }
