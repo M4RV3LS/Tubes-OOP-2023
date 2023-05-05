20ce9816fa03ee6b2e2223a142ce83a272a21c5a
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2023-05-06 00:44:56 +0700

    Update Aksi.java

--- Aksi.java
+++ Aksi.java
@@ -1,12 +1,19 @@
 package Sim;
-import Sim.*;
 
 public interface Aksi {
-    void olahraga();
-    void tidur();
-    void masak();
-    void makan();
-    int buangAir();
-    void lihatWaktu();
-    void mainGame(lamaMain);
+    public int kerja(int lamaKerja);
+    public void olahraga();
+    public void tidur();
+    public void masak();
+    public void makan();
+    public int buangAir();
+    public void lihatWaktu();
+    public void mainGame(int lamaMain);
+    public void santet(Sim simLain);
+    public void berobat(int lamaBerobat);
+    public void karaoke(int lamaKaraoke);
+    public void puasa();
+    public void bersihBersih(int lamaBersihBersih);
+    public void melawak();
+    public void moveToObject(int x, int y);
 }
