3ec6e5f6c332a31724a7ec06a0dcb01b39915abc
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2023-05-06 00:43:15 +0700

    interface aksi

---
+++ Aksi.java
@@ -0,0 +1,12 @@
+package Sim;
+import Sim.*;
+
+public interface Aksi {
+    void olahraga();
+    void tidur();
+    void masak();
+    void makan();
+    int buangAir();
+    void lihatWaktu();
+    void mainGame(lamaMain);
+}
